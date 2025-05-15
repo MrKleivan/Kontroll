52b9213a6713765c682c5d1eb66ee791746f7a4d
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2025-05-15 15:00:19 +0200

    vbnvgg

--- Kontroll.Frontend/src/App.vue
+++ Kontroll.Frontend/src/App.vue
@@ -1,25 +1,57 @@
 <script setup>
+import { ref, Transition } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 import { colorMode, toggleColorMode } from './components/ColorMode';
+const isMenuVisible = ref(false);
+const toggleMenu  = () => {
+  isMenuVisible.value = !isMenuVisible.value;
+};
+let hideTimeout = null;
 
-const ToggleColorMode = toggleColorMode;
+const hideMenuWithDelay = () => {
+  hideTimeout = setTimeout(() => {
+    isMenuVisible.value = false;
+  }, 300); // 300 ms delay
+};
+
+const cancelHide = () => {
+  clearTimeout(hideTimeout);
+};
+
 </script>
 
 <template>
   <header>
-
-    <div :class="colorMode.value">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <button @click="toggleColorMode" variant="colorMode"> Color mode</button>
-      </nav>
+    <div class="headerLeft">
+      <div class="headerLogo">
+  
+      </div>
+    </div>
+    <div class="headerRight">
+      <div class="headerMenu">
+        <span>Meny </span>
+        <button id="MenuButton" @click="toggleMenu">&#9776;</button>
+      </div>
+      <div class="mainNavConteiner">
+        <nav class="MainNav" v-show="isMenuVisible" @mouseleave="hideMenuWithDelay" @mouseenter="cancelHide">
+          <RouterLink class="mainNavButton" to="/">Hjem</RouterLink>
+          <RouterLink class="mainNavButton" to="/myProfile">Profil</RouterLink>
+          <RouterLink class="mainNavButton" to="/mySettings">Innstillinger</RouterLink>
+          <RouterLink class="mainNavButton" to="/loggOut">Logg ut</RouterLink>
+          <button class="mainNavButton" @click="toggleColorMode" variant="colorMode"> Color mode</button>
+        </nav>
+      </div>
     </div>
   </header>
 
   <RouterView />
+
+  <footer>
+
+  </footer>
 </template>
 
 <style scoped>
+
 
 </style>
--- Kontroll.Frontend/src/views/HomeView.vue
+++ Kontroll.Frontend/src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<main>
+  <div class="contentWindow">
+    <div class="contentWindowHeader">
+        <nav class="contentWindowNavBar">
+            <RouterLink class="contentWindowNav" to="/">Transaksjoner</RouterLink>
+            <RouterLink class="contentWindowNav" to="/">Faste Utgifter</RouterLink>
+            <RouterLink class="contentWindowNav" to="/">Fakturaoversikt</RouterLink>
+            <RouterLink class="contentWindowNav" to="/">Leverandøroversikt</RouterLink>
+        </nav>
+    </div>
+    <div class="contentWindowContent">
+
+    </div>
+  </div>
+  <div class="infoStripe">
+    <div class="UserInfoConteiner">
+        <div class="UserInfoConteinerHeader">
+          <span>Per Ivar Kleivan</span>
+        </div>
+        <div class="UserInfoConteinerContent">
+          <span></span>
+        </div>
+    </div>
+    <br />
+    <div class="UserInfoConteiner">
+
+    </div>
+  </div>
+</main>
 </template>
